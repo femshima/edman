a0155c40f5d158bba52612661bf89a0521e6857c
femshima <49227365+femshima@users.noreply.github.com>
2023-09-08 19:32:32 +0900

    add config

--- crates/edman-ui/src/page/config.rs
+++ crates/edman-ui/src/page/config.rs
@@ -1,22 +1,79 @@
-use iced::{widget::column, Command, Element};
-use tonic::{transport::Channel, Request, Response};
+use iced::{
+    widget::{column, row, text, text_input},
+    Command, Element,
+};
+use tonic::Request;
 
 use crate::grpc;
 
-pub struct ConfigSettings {}
+pub struct ConfigSettings {
+    config: Option<grpc::config::Config>,
+}
 
+#[derive(Debug, Clone)]
 pub enum ConfigSettingsMessage {
-    Loaded(Result<Response<grpc::ui::ConfigReply>, tonic::Status>),
+    Loaded(Result<Option<grpc::config::Config>, tonic::Status>),
 }
 
 impl ConfigSettings {
-    // pub fn new(client: &mut grpc::Client) -> (Self, Command<ConfigSettingsMessage>) {
-    //     Self {}
-    // }
+    pub fn new(channel: transport::GrpcChannel) -> (Self, Command<ConfigSettingsMessage>) {
+        let client = grpc::Client::new(channel.clone());
 
-    pub fn view(&self) -> Element<ConfigSettingsMessage> {
-        column![].into()
+        (
+            Self { config: None },
+            Command::perform(Self::fetch_files(client), ConfigSettingsMessage::Loaded),
+        )
     }
 
-    pub fn update(&mut self, message: ConfigSettingsMessage) {}
+    pub fn view(&self) -> Element<ConfigSettingsMessage> {
+        let Some(ref config)= self.config else {
+            return text("loading").into()
+        };
+
+        column![
+            "Download",
+            row![
+                text_input("base dir", &config.download_directory[..]),
+                "/",
+                text_input("sub dir", &config.download_subdirectory[..])
+            ],
+            "Save",
+            text_input("save dir", &config.save_file_directory[..]),
+            "Ext[Cr]",
+            column(
+                config
+                    .allowed_origins
+                    .iter()
+                    .map(|s| text(s).into())
+                    .collect()
+            ),
+            "Ext[FF]",
+            column(
+                config
+                    .allowed_extensions
+                    .iter()
+                    .map(|s| text(s).into())
+                    .collect()
+            )
+        ]
+        .into()
+    }
+
+    pub fn update(&mut self, message: ConfigSettingsMessage) {
+        match message {
+            ConfigSettingsMessage::Loaded(result) => match result {
+                Ok(config) => self.config = config,
+                Err(err) => eprintln!("{}", err),
+            },
+        }
+    }
+
+    async fn fetch_files(
+        mut client: grpc::Client,
+    ) -> Result<Option<grpc::config::Config>, tonic::Status> {
+        let response = client
+            .get_config(Request::new(grpc::ui::ConfigRequest {}))
+            .await?;
+        Ok(response.get_ref().config.to_owned())
+    }
 }
--- crates/edman-ui/src/page/mod.rs
+++ crates/edman-ui/src/page/mod.rs
@@ -1,29 +1,33 @@
 mod config;
 mod files;
 
-use iced::widget::{column, scrollable};
+use iced::widget::{column, row, scrollable};
 use iced::{Alignment, Command, Element};
 
-// use self::config::{ConfigSettings, ConfigSettingsMessage};
+use self::config::{ConfigSettings, ConfigSettingsMessage};
 use self::files::{FileView, FileViewMessage};
 
 pub struct Page {
     file_view: FileView,
-    // configs: ConfigSettings,
+    configs: ConfigSettings,
 }
 
 #[derive(Debug, Clone)]
 pub enum PageMessage {
     FileMessage(FileViewMessage),
-    // ConfigMessage(ConfigSettingsMessage),
+    ConfigMessage(ConfigSettingsMessage),
 }
 
 impl Page {
     pub fn new(grpc_channel: transport::GrpcChannel) -> (Self, Command<PageMessage>) {
         let (file_view, file_view_command) = FileView::new(grpc_channel.clone());
+        let (configs, config_command) = ConfigSettings::new(grpc_channel.clone());
         (
-            Self { file_view },
-            file_view_command.map(PageMessage::FileMessage),
+            Self { file_view, configs },
+            Command::batch(vec![
+                file_view_command.map(PageMessage::FileMessage),
+                config_command.map(PageMessage::ConfigMessage),
+            ]),
         )
     }
 
@@ -31,6 +35,9 @@ impl Page {
         match message {
             PageMessage::FileMessage(m) => {
                 self.file_view.update(m);
+            }
+            PageMessage::ConfigMessage(m) => {
+                self.configs.update(m);
             }
         }
 
@@ -40,6 +47,10 @@ impl Page {
     pub fn view(&self) -> Element<PageMessage> {
         let files: Element<_> = self.file_view.view().map(PageMessage::FileMessage);
 
-        scrollable(column![files,].padding(20).align_items(Alignment::Center)).into()
+        row![
+            scrollable(column![files,].padding(20).align_items(Alignment::Center)),
+            self.configs.view().map(PageMessage::ConfigMessage)
+        ]
+        .into()
     }
 }
